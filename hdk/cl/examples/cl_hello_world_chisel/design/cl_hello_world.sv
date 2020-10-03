846ad93bc98f064c936dbb4c10204dfdd4520262
HORIE Tetsuya <horie-t@users.noreply.github.com>
2020-10-03 19:00:06 +0900

    cl_hello_worldのコア部分を別ファイルに抽出

--- hdk/cl/examples/cl_hello_world_chisel/design/cl_hello_world.sv
+++ hdk/cl/examples/cl_hello_world_chisel/design/cl_hello_world.sv
@@ -52,10 +52,10 @@ logic rst_main_n_sync;
   logic [31:0] araddr_q;
   logic [31:0] hello_world_q_byte_swapped;
   logic [15:0] vled_q;
-  logic [15:0] pre_cl_sh_status_vled;
-  logic [15:0] sh_cl_status_vdip_q;
-  logic [15:0] sh_cl_status_vdip_q2;
-  logic [31:0] hello_world_q;
+  //logic [15:0] pre_cl_sh_status_vled;
+  //logic [15:0] sh_cl_status_vdip_q;
+  //logic [15:0] sh_cl_status_vdip_q2;
+  //logic [31:0] hello_world_q;
 
 //-------------------------------------------------
 // ID Values (cl_hello_world_defines.vh)
@@ -262,57 +262,14 @@ always_ff @(posedge clk_main_a0)
       rresp  <= 0;
    end
 
-//-------------------------------------------------
-// Hello World Register
-//-------------------------------------------------
-// When read it, returns the byte-flipped value.
-
-always_ff @(posedge clk_main_a0)
-   if (!rst_main_n_sync) begin                    // Reset
-      hello_world_q[31:0] <= 32'h0000_0000;
-   end
-   else if (wready & (wr_addr == `HELLO_WORLD_REG_ADDR)) begin  
-      hello_world_q[31:0] <= wdata[31:0];
-   end
-   else begin                                // Hold Value
-      hello_world_q[31:0] <= hello_world_q[31:0];
-   end
-
-assign hello_world_q_byte_swapped[31:0] = {hello_world_q[7:0],   hello_world_q[15:8],
-                                           hello_world_q[23:16], hello_world_q[31:24]};
-
-//-------------------------------------------------
-// Virtual LED Register
-//-------------------------------------------------
-// Flop/synchronize interface signals
-always_ff @(posedge clk_main_a0)
-   if (!rst_main_n_sync) begin                    // Reset
-      sh_cl_status_vdip_q[15:0]  <= 16'h0000;
-      sh_cl_status_vdip_q2[15:0] <= 16'h0000;
-      cl_sh_status_vled[15:0]    <= 16'h0000;
-   end
-   else begin
-      sh_cl_status_vdip_q[15:0]  <= sh_cl_status_vdip[15:0];
-      sh_cl_status_vdip_q2[15:0] <= sh_cl_status_vdip_q[15:0];
-      cl_sh_status_vled[15:0]    <= pre_cl_sh_status_vled[15:0];
-   end
-
-// The register contains 16 read-only bits corresponding to 16 LED's.
-// For this example, the virtual LED register shadows the hello_world
-// register.
-// The same LED values can be read from the CL to Shell interface
-// by using the linux FPGA tool: $ fpga-get-virtual-led -S 0
-
-always_ff @(posedge clk_main_a0)
-   if (!rst_main_n_sync) begin                    // Reset
-      vled_q[15:0] <= 16'h0000;
-   end
-   else begin
-      vled_q[15:0] <= hello_world_q[15:0];
-   end
-
-// The Virtual LED outputs will be masked with the Virtual DIP switches.
-assign pre_cl_sh_status_vled[15:0] = vled_q[15:0] & sh_cl_status_vdip_q2[15:0];
+   cl_hello_world_core CL_HELLO_WORLD_CORE(clk_main_a0,
+					   rst_main_n_sync,
+					   wr_addr,
+					   wready,
+					   hello_world_q,
+					   hello_world_q_byte_swapped,
+					   cl_sh_status_vled,
+					   vled_q);
 
 //-------------------------------------------
 // Tie-Off Unused Global Signals
