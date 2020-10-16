371f73608d1b7faff891ab6405b7996167df7bc5
HORIE Tetsuya <horie-t@users.noreply.github.com>
2020-10-16 21:01:14 +0900

    Chisel部分のI/FをAXI Liteに変更し、読み出し部分を実装

--- hdk/cl/examples/cl_hello_world_chisel/chisel_design/src/main/scala/ClHelloWorldCore.scala
+++ hdk/cl/examples/cl_hello_world_chisel/chisel_design/src/main/scala/ClHelloWorldCore.scala
@@ -1,7 +1,7 @@
 import chisel3._
 import chisel3.util._
 
-class ClHelloWorldCore extends Module {
+class ClHelloWorldCore extends MultiIOModule {
   val io = IO(new Bundle {
     val wrAddr = Input(UInt(32.W))
     val wData = Input(UInt(32.W))
@@ -12,7 +12,57 @@ class ClHelloWorldCore extends Module {
     val vLed = Output(UInt(16.W))
   })
 
+  //// 入力
+  // 書き込み
+  val awwarid = IO(Input(Bool()))
+  val awadder = IO(Input(UInt(32.W)))
+  val wvalid = IO(Input(Bool()))
+  val wstrb = IO(Input(UInt(4.W)))
+  val bready = IO(Input(Bool()))
+  // 読み出し
+  val arvaild = IO(Input(Bool()))
+  val araddr = IO(Input(UInt(32.W)))
+  val rready = IO(Input(Bool()))
+
+  //// 出力
+  // 書き込み
+  val awready = IO(Output(Bool()))
+  val wready = IO(Output(Bool()))
+  val bvalid = IO(Output(Bool()))
+  val bresp = IO(Output(UInt(2.W)))
+  // 読み出し
+  val arready = IO(Output(Bool()))
+  val rvalid = IO(Output(Bool()))
+  val rdata = IO(Output(UInt(32.W)))
+  val rresp = IO(Output(UInt(2.W)))
+
   val HELLO_WORLD_REG_ADDR = "h0000_0500".U(32.W)
+
+  /*
+   * 読み出し
+   */
+  /* 読み出しステート定義
+   * アイドル、アドレス・準備完了、データ有効 */
+  val sReadIdle :: sReadAddrReady :: sReadDateValid:: Nil = Enum(3)
+  val readStateReg = RegInit(sReadIdle)
+
+  //ステートマシン
+  val readAddrReg = RegInit(0.U(32.W))
+  when (readStateReg === sReadIdle && arvaild) {
+    readStateReg := sReadAddrReady
+    readAddrReg := araddr
+  } .elsewhen (readStateReg === sReadAddrReady) {
+    readStateReg := sReadDateValid
+  } .elsewhen (readStateReg === sReadDateValid && rready) {
+    readStateReg := sReadIdle
+  }
+
+  // 出力
+  arready := readStateReg === sReadAddrReady
+  rvalid := readStateReg === sReadDateValid
+  rdata := Mux(readAddrReg === HELLO_WORLD_REG_ADDR,
+    Cat(helloWorldQ(15, 0), helloWorldQ(31, 16)),
+    Cat(0.U(16.W), vLedQ))
 
   //-------------------------------------------------
   // Hello World Register
